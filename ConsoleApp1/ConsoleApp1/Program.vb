45ef9cce5c0f5a886efccc544d1f8c0f2cb741c1
Marcos Brioso <marcosbriosobenavides2002@gmail.com>
2022-07-25 14:23:04 -0600

    hello world

---
+++ ConsoleApp1/ConsoleApp1/Program.vb
@@ -0,0 +1,11 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim A As String
+        Console.WriteLine("escribe tu nombre por favor ")
+        A = Console.ReadLine()
+        Console.WriteLine("hola mundo " + A)
+
+    End Sub
+End Module
